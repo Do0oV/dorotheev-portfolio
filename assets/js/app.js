b7b948676b1ca7cf0016b84b57905f7050442d6e
Do0oV <viard.dorothee@gmail.com>
2018-08-05 19:02:28 +0200

    sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 require('../sass/app.scss');
 
+import '../js/admin.js';
+import '../js/service-worker.js';
 
 $(function() {
 	$('#resume-list').hide();
@@ -45,45 +47,7 @@ $(function() {
 
 })
 
-//SCROLL BY 100VH
-$(document).ready(function () {
-    var divs = $('.mydiv');
-    var dir = 'up'; // wheel scroll direction
-    var div = 0; // current div
-    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
-        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-            dir = 'down';
-        } else {
-            dir = 'up';
-        }
-        // find currently visible div :
-        div = -1;
-        divs.each(function(i){
-            if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
-                div = i;
-            }
-        });
-        if (dir == 'up' && div > 0) {
-            div--;
-        }
-        if (dir == 'down' && div < divs.length) {
-            div++;
-        }
-        //console.log(div, dir, divs.length);
-        $('html,body').stop().animate({
-            scrollTop: divs.eq(div).offset().top
-        }, 1000);
-        return false;
-    });
 
-    //BACK TO TOP
-//     $(window).resize(function () {
-//         $('html,body').scrollTop(divs.eq(div).offset().top);
-//     });
-
-
-
-});
 
 
 var $button = document.querySelector('.btn-send');
@@ -94,3 +58,10 @@ $button.addEventListener('click', function() {
 	TweenMax.to($button, duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams: [3], delay: delay});
 	TweenMax.to($button, duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams: [6], delay: delay * 3 });
 });
+
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('../js/service-worker.js')
+           .then(function() { console.log("Service Worker Registered"); });
+}
